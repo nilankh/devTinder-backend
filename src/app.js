a763c92926298ac575cbb1ca128ce39b2b3e2a91
nilank nikhil <nilanknikhil@gmail.com>
2024-12-09 02:14:32 +0530

    installed the nodemon and basic thing

--- src/app.js
+++ src/app.js
@@ -4,4 +4,11 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000);
+app.use("/test",(req, res) => {
+    res.send("Hello from the server");
+})
+
+
+app.listen(3000, ()=> {
+    console.log("Server is successfully listenting..");
+});
