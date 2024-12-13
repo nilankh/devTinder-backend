40310075d771c7afeadbee0b442de39ba14c812f
nilank nikhil <nilanknikhil@gmail.com>
2024-12-13 20:24:16 +0530

    routing concepts

--- src/app.js
+++ src/app.js
@@ -4,10 +4,28 @@ const express = require("express");
 
 const app = express();
 
+
+// if line 9 "/" we are uncommeting and callingthen why /nilank or /test is not working, reason is we have associated / with the  the given data
+// app.use("/",(req, res) => {
+//     res.send("Hello from the server");
+// })
+
 app.use("/test",(req, res) => {
-    res.send("Hello from the server");
+    res.send("Hello from test");
 })
 
+app.use("/test/2",(req, res) => {
+    // this will not work, because /test is already defined, so whenever /test will hit, it will redirect to aboeve line
+    res.send("Hello from test2");
+})
+
+app.use("/nilank",(req, res) => {
+    res.send("Hello from the nilank");
+})
+
+app.use("/",(req, res) => {
+    res.send("Hello from the server");
+})
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listenting..");
