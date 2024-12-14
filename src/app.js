567edff843b9975e208866e99a056f321c6b3e46
nilank nikhil <nilanknikhil@gmail.com>
2024-12-14 18:49:58 +0530

    routung and request handler

--- src/app.js
+++ src/app.js
@@ -11,22 +11,37 @@ const app = express();
 //     res.send("Hello from the server");
 // })
 
+app.use("/user", (req, res) => {
+    res.send("HHAHAHAHHAHH")
+})
+
+// THis will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({"firstName":"Nilank", "lastName":"NIKHIL"})
+})
+
+app.post("/user", (req, res) => {
+    // assume saving it to db
+    res.send("Data Successfully save to the database.")
+})
+
+// this will match all the HTTP method API calls to /test
 app.use("/test",(req, res) => {
     res.send("Hello from test");
 })
 
-app.use("/test/2",(req, res) => {
-    // this will not work, because /test is already defined, so whenever /test will hit, it will redirect to aboeve line
-    res.send("Hello from test2");
-})
+// app.use("/test/2",(req, res) => {
+//     // this will not work, because /test is already defined, so whenever /test will hit, it will redirect to aboeve line
+//     res.send("Hello from test2");
+// })
 
-app.use("/nilank",(req, res) => {
-    res.send("Hello from the nilank");
-})
+// app.use("/nilank",(req, res) => {
+//     res.send("Hello from the nilank");
+// })
 
-app.use("/",(req, res) => {
-    res.send("Hello from the server");
-})
+// app.use("/",(req, res) => {
+//     res.send("Hello from the server");
+// })
 
 app.listen(3000, ()=> {
     console.log("Server is successfully listenting..");
