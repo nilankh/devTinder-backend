93242c339bad0f5190ef77b14d01199831cac650
nilank nikhil <nilanknikhil@gmail.com>
2024-12-15 18:02:23 +0530

    middleware error handling

--- src/app.js
+++ src/app.js
@@ -3,30 +3,24 @@ const express = require("express");
 // create new express application, instance of express
 const app = express();
 
-const {adminAuth, userAuth} = require("./middlewares/auth")
-
-app.use("/admin", adminAuth)
-
-app.post("/user/login", (req, res) => {
-    res.send("User logged in successfully!!")
+app.get("/getUserData",(req, res) => {
+    // logic of db call and get some user data, suppose som eeroror happens, or you can use try and catch
+    try {
+        throw new Error("Nilank error")
+        res.send("User Data sent!")
+    } catch (error) {
+        res.status(500).send("Something went wrong!!")
+    }
+   
 })
 
-
-// app.use("/user", userAuth) or we can write in a different way
-app.get("/user", userAuth, (req, res) => {
-    res.send("User Data sent!")
+//err should be the first parameter.
+app.use("/", (err, req, res, next) => {
+    if(err){
+        // log your error message
+        res.status(500).send("Something went wrong!!")
+    }
 })
-
-//after makoign middleware we can directly send the response back
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All Data sent!")
-})
-
-app.get('/admin/deleteUser', (req,res) => {
-    res.send("User is deleted")
-})
-
-
 app.listen(3000, ()=> {
     console.log("Server is successfully listenting..");
 });
