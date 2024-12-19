17e38cb8b949d29157420be2ef50657c53f2447d
nilank nikhil <nilanknikhil@gmail.com>
2024-12-19 20:52:22 +0530

    first conneced with datbase then server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const connectDB = require("./config/database")
 require("./config/database")
 // create new express application, instance of express
 const app = express();
 
 
 
+connectDB().then(() => {
+    console.log("Database connection eastablished....")
+    app.listen(3000, ()=> {
+        console.log("Server is successfully listenting..");
+    });
+}).catch(err => {
+    console.log("Database can not be connected..")
+})
 
+// app.listen(3000, ()=> {
+//     console.log("Server is successfully listenting..");
+// });
 
-app.listen(3000, ()=> {
-    console.log("Server is successfully listenting..");
-});
+// write way is first connect way to db then start the server
