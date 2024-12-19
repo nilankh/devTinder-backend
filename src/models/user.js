4a9f92a89061bbb2228f3eeea996cd7efd6a2993
nilank nikhil <nilanknikhil@gmail.com>
2024-12-19 21:02:01 +0530

    Create a user schema

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { useFormStatus } = require('react-dom');
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    emailId: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    }
+})
+
+// creating a model
+const User = mongoose.model("User", userSchema);
+
+module.exports = useFormStatus;
