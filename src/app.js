205c61fbc9b62fa07bd073bc13b14639771129d8
nilank nikhil <nilanknikhil@gmail.com>
2024-12-25 18:46:42 +0530

    learned about why express.json is required

--- src/app.js
+++ src/app.js
@@ -4,24 +4,29 @@ const connectDB = require("./config/database")
 // create new express application, instance of express
 const app = express();
 const User = require("./models/user")
-console.log("fszd",User)
+
+// now this middleware will be active for all the routes
+app.use(express.json());
 
 app.post('/signup', async(req, res) => {
-    const userObj = {
-        firstName: "Harshita",
-        lastName: "Annadanapu",
-        emailId: "harshita@gmail.com",
-        password: "Nilank@123"
-    }
-    try{
-        // creating new Instance of User model
-        const user = new User(userObj);
-        // it returns a promises so we need to await
-        await user.save();
-        res.send("User successfully created!");
-    } catch(err) {
-        res.status(400).send("Error saving the user: ",err.message);
-    }
+
+    // it will be undefined, reason is our server is not able to read the json data, to read the json data we need a middleware which can check the incoming request and parse the json data
+    console.log("req", req.body)
+    // const userObj = {
+    //     firstName: "Neel",
+    //     lastName: "Punj",
+    //     emailId: "punjneel@gmail.com",
+    //     password: "Nilank@123"
+    // }
+    // try{
+    //     // creating new Instance of User model
+    //     const user = new User(userObj);
+    //     // it returns a promises so we need to await
+    //     await user.save();
+    //     res.send("User successfully created!");
+    // } catch(err) {
+    //     res.status(400).send("Error saving the user: ",err.message);
+    // }
 });
 
 
