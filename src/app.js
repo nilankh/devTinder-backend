85198238ee1d935a03660cd823f0976b4d5ef58f
nilank nikhil <nilanknikhil@gmail.com>
2024-12-25 17:38:28 +0530

    database schema and models

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const connectDB = require("./config/database")
-require("./config/database")
+// require("./config/database")
 // create new express application, instance of express
 const app = express();
+const User = require("./models/user")
+console.log("fszd",User)
 
+app.post('/signup', async(req, res) => {
+    const userObj = {
+        firstName: "Nilank",
+        lastName: "Nikhil",
+        emailId: "nilanknikhil@gmail.com",
+        password: "Nilank@123"
+    }
+
+    // creating new Instance of User model
+    const user = new User(userObj);
+    // it returns a promises so we need to await
+    await user.save();
+
+    res.send("User successfully created!")
+
+})
 
 
 connectDB().then(() => {
@@ -19,4 +37,4 @@ connectDB().then(() => {
 //     console.log("Server is successfully listenting..");
 // });
 
-// write way is first connect way to db then start the server
+// right way is first connect way to db then start the server
