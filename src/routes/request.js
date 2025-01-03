351ec421aa5add3c7f65abf7a5e5c8e76a54b54d
nilank nikhil <nilanknikhil@gmail.com>
2025-01-03 23:28:21 +0530

    create connection request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,13 +2,28 @@ const express = require('express');
 const requestRouter = express.Router();
 
 const {userAuth} = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 
+requestRouter.post("/request/send/:status/:toUserId", userAuth,async(req, res) => {
+    try{
+        const fromUserId = req.user._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
 
-requestRouter.post("/sendConnectionRquest", userAuth,async(req, res) => {
-    // send a connection request to the user
-    console.log("send a connection request to the user");
+        const connectionRequest = new ConnectionRequest({
+            fromUserId,
+            toUserId,
+            status
+        });
 
-    res.send("Connection request sent successfully");
+        const data = await connectionRequest.save();
+
+        res.json({"message":"Connection Request Sent Successfully", data});
+
+    }
+    catch(err){
+        res.status(400).send("Error in sending request: " + err.message);
+    }
 });
 
 module.exports = requestRouter;
