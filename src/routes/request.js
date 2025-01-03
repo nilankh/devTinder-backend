a8bb4aa076061d63c38b79709e26a9001a779d17
nilank nikhil <nilanknikhil@gmail.com>
2025-01-04 03:01:10 +0530

    validation on connection request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -10,6 +10,25 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth,async(req, res) =
         const toUserId = req.params.toUserId;
         const status = req.params.status;
 
+        const allowedStatus = ['ignored','interested']
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({"message":`Invalid status type: ${status}` });
+        }
+
+
+        // IF there is an existing ConnectionRequest
+        const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or:[
+            {fromUserId, toUserId},
+            {fromUserId: toUserId, toUserId: fromUserId},
+            ]
+        });
+
+        if(existingConnectionRequest){
+            return res.status(400).json({"message":"Connection Request Already Exists"});
+        }
+
+
         const connectionRequest = new ConnectionRequest({
             fromUserId,
             toUserId,
