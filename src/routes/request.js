d265bfed1b5c55eaceae1a7b0c5e014049c913f1
nilank nikhil <nilanknikhil@gmail.com>
2025-01-04 17:21:26 +0530

    index and compound index

--- src/routes/request.js
+++ src/routes/request.js
@@ -47,7 +47,7 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth,async(req, res) =
 
         const data = await connectionRequest.save();
 
-        res.json({"message":"Connection Request Sent Successfully", data});
+        res.json({"message":req.user.firstName + " is " + status +" in " + toUser.firstName, data});
 
     }
     catch(err){
